b49569ba9cc1a14b02d5190855c6e5724ad3cee1
Johana <johanatorres91>
2019-09-02 12:39:19 -0500

    se cambio el fondo a azul

---
+++ ST1619_Practice10_U2_V1/inicio.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+  <link rel="stylesheet" type="text/css" href="index.css">
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <div class="transparency">
+      <div class="text">
+    <?php
+    $name=$_GET["user"];
+    echo "Welcome to Twitter". $name."!";
+     ?>
+  </body>
+</html>
